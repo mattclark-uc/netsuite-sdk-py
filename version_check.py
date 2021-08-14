19ba083ede9ff230b516fa6aae655397205bccad
mattclark-uc <mattc@undergroundcellar.com>
2021-08-13 17:16:37 -0700

    oops

---
+++ version_check.py
@@ -0,0 +1,5 @@
+from version import VERSION as NEW_VERSION
+from old_version import VERSION as OLD_VERSION
+
+if NEW_VERSION == OLD_VERSION:
+	print('##[error] You must change the version number in version.py')
