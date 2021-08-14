a01d8cb28a6c86d85530953109f6953a748e259c
mattclark-uc <mattc@undergroundcellar.com>
2021-08-13 17:24:44 -0700

    hmm

--- version_check.py
+++ version_check.py
@@ -2,4 +2,4 @@ from version import VERSION as NEW_VERSION
 from old_version import VERSION as OLD_VERSION
 
 if NEW_VERSION == OLD_VERSION:
-	print('##vso[task.logissue]error You must change the version number in version.py')
+	print('##vso[task.logissue type=error] You must change the version number in version.py')
